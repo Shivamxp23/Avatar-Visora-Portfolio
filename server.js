f917d45f8b3723ec21dcc3bfc30495f2ec83e1bc
Shivam <soni820034@gmail.com>
2026-09-14 20:19:06 +0530

    Set 07BdkAFaowgJjdCtretGQDJJM.ico as website favicon across all pages

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const MIME_TYPES = {
   '.gif': 'image/gif',
   '.webp': 'image/webp',
   '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
   '.woff': 'font/woff',
   '.woff2': 'font/woff2',
   '.ttf': 'font/ttf',
